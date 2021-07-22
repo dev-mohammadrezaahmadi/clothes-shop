068bc0c4790ae5bd2bf1bf14b3a4542cd762ce4c
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-07-22 21:06:47 +0430

    add methods and functions to have persistent authentication through the whole app with firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,47 @@ import firebase from 'firebase/app'
 import React from 'react'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/Layout.component'
-import { auth } from '../firebase/firebase.utils'
+import { auth, createUserProfileDocument } from '../firebase/firebase.utils'
 import { UserContext } from '../context/UserContext'
+import { Toaster } from 'react-hot-toast'
 
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [currentUser, setCurrentUser] = React.useState<firebase.User | null>(null)
+  const [currentUser, setCurrentUser] = React.useState<any>(null)
   React.useEffect(() => {
-    let unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user)
+    let unsubscribe = auth.onAuthStateChanged(async user => {
+
+      if (user) {
+        const userRef = await createUserProfileDocument(user)
+
+        userRef?.onSnapshot(snapshot => {
+          setCurrentUser({
+            ...currentUser,
+            id: snapshot.id,
+            ...snapshot.data()
+          })
+        })
+      } else {
+        setCurrentUser(user)
+      }
+
     })
+
+
     return () => {
       unsubscribe()
     }
   }, [])
 
+  // React.useEffect(() => {
+  //   console.log(currentUser)
+  // }, [currentUser])
+
   return (
     <>
+      <Toaster />
       <UserContext.Provider value={{ user: currentUser }}>
         <Layout>
           <Component {...pageProps} />
