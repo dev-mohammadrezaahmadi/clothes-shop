190ddd9e525a27f8745fca296fd39a6eb7cdaab2
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-07-29 01:29:59 +0430

    add nprogress lib to have better ui when fetching data route to route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,13 @@ import { store } from '../redux/store'
 import Layout from '../components/layout/Layout'
 import AuthProvider from './api/auth'
 import '../styles/globals.css'
-
+import { Router } from 'next/router'
+import NProgress from "nprogress"
+import "nprogress/nprogress.css"
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
