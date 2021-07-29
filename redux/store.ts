e36e0665345cdd4fea5db5957ff41daef28f38e8
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-07-30 01:24:38 +0430

    configure redux store to utilize redux-saga

--- redux/store.ts
+++ redux/store.ts
@@ -1,17 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import userReducer from "./slices/user.reducer";
+import rootSaga from "./sagas/rootSaga";
 import cartReducer from "./slices/cart.reducer";
 import directoryReducer from "./slices/directory.reducer";
 import shopReducer from "./slices/shop.reducer";
 
-export const store = configureStore({
-	reducer: {
-		user: userReducer,
-		cart: cartReducer,
-		directory: directoryReducer,
-		shop: shopReducer,
-	},
+const sagaMiddleware = createSagaMiddleware();
+
+const reducer = combineReducers({
+	user: userReducer,
+	cart: cartReducer,
+	directory: directoryReducer,
+	shop: shopReducer,
 });
+
+export const store = configureStore({
+	reducer,
+	middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
