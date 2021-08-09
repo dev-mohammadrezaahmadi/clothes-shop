6970a81708cfac903754400b46ce9057c27faecc
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-10 00:52:25 +0430

    fix some issues with firebase config

--- firebase/firebase.utils.js
+++ firebase/firebase.utils.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	authDomain: "clothes-shop-database.firebaseapp.com",
-	projectId: "clothes-shop-database",
-	storageBucket: "clothes-shop-database.appspot.com",
-	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_APP_ID,
+	apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+	authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
+	projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+	storageBucket: `${process.env.NEXT_PUBLIC_STORAGE_BUCKET}`,
+	messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID}`,
+	appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
 };
 
 if (!firebase.apps.length) {
